f90ffeffdfc37911a45f028f9f3f898aaa23677b
meenu10802 <meenu10802>
2025-12-18 08:40:27 +0530

    Added product code in develop branch

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CgTraining2
+{
+    internal class Product
+    {
+       public static void Developing()
+        {
+            Console.WriteLine("Improving a product");
+            Console.WriteLine("There is a urgent fix in login");
+        }
+
+    }
+}
