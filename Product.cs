b0001e84be6c5341b4086b5556dbe77b0f02af29
meenu10802 <meenu10802>
2025-12-18 08:46:06 +0530

    Added more code after copying hash in hotfix/login  for cherry pick task

--- Product.cs
+++ Product.cs
@@ -13,6 +13,7 @@ namespace CgTraining2.CgTraining2
             Console.WriteLine("Improving a product");
             Console.WriteLine("There is a urgent fix in login that is needed in developing right now");
             Console.WriteLine("Fixing bug in hotfix");
+            Console.WriteLine("After fixing and committing i added this line this will not refelct in develop because cherry pciked");
         }
 
     }
