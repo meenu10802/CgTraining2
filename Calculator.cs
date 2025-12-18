b7a740fb3995d72849f5483e6a6e587c5265f261
meenu10802 <meenu10802>
2025-12-18 08:33:03 +0530

    added calculator code

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Addition: " + (n1 + n2));
+        Console.WriteLine("Subtraction: " + (n1 - n2));
+    }
+}
