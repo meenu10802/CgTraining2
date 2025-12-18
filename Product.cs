691d333b9ef5159eae32d0d91dedd8e23ffe35e5
meenu10802 <meenu10802>
2025-12-18 08:42:02 +0530

    Fixed bug in hotfix/login

--- Product.cs
+++ Product.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CgTraining2
+namespace CgTraining2.CgTraining2
 {
     internal class Product
     {
        public static void Developing()
         {
             Console.WriteLine("Improving a product");
-            Console.WriteLine("There is a urgent fix in login");
+            Console.WriteLine("There is a urgent fix in login that is needed in developing right now");
+            Console.WriteLine("Fixing bug in hotfix");
         }
 
     }
